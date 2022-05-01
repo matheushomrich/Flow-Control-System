523b35e56b060ef3dedaa77714b6b39d1de3d1d3
TGVM <tgvm96@gmail.com>
2022-05-01 13:48:33 -0300

    comentarios saw

--- flowctrlsim.py
+++ flowctrlsim.py
@@ -2,107 +2,79 @@
 #Feito pela dupla Matheus Homrich e Thiago Mello
 
 import sys
-from tkinter import Frame
+
 
 # Stop-and-Wait ARQ
 def saw(num_frames, lost_pkts):
     # Emissor envia 1 frame, receptor tenta receber, se não houver erros, receptor envia ack
     #se houver erros => Timeout e emissor tenta novamente
-    frame = False
-    c = 0
-    if int(lost_pkts[0]) == 0:
-        for i in range(1, num_frames+1):
-            if frame:
-                print("A ->> B : ("+str(i)+") Frame 1")
-                print("B -->> A : Ack 0")
-            else:
-                print("A ->> B : ("+str(i)+") Frame 0")
-                print("B -->> A : Ack 1")
-            frame = not frame
-    elif (len(lost_pkts)==1):
-        i = 1
-        while i <= num_frames:
-            if frame:
-                print("A ->> B : ("+str(i)+") Frame 1") # falta arrumar o print "A -x B" caso a transmissão tenha erro
-                if(len(lost_pkts) > c):
-                    if(i == int(lost_pkts[c])):
-                        print("Note over A : TIMEOUT ("+str(i)+")")
-                        i-=1
-                        c+=1
-                        frame = not frame
-                    else:
-                        print("B -->> A : Ack 0")
-                else:
-                    print("B -->> A : Ack 0")
-            else:
-                print("A ->> B : ("+str(i)+") Frame 0")
-                if(len(lost_pkts) > c):
-                    if(i == int(lost_pkts[c])):
-                        print("Note over A : TIMEOUT ("+str(i)+")")
-                        i=i-1
-                        c+=1
-                        frame = not frame
-                    else:
-                        print("B -->> A : Ack 1")
-                else:
-                    print("B -->> A : Ack 1")
-            frame = not frame
-            i+=1
+
+    # define a quantidade de mensagens que devem aparecer na tela
+    # numero de frames + quantidade de erros * 2
+    if lost_pkts[0] == "0":
+       transmission = ["x"] * (((num_frames) * 2) + 1)
     else:
         transmission = ["x"] * (((num_frames + len(lost_pkts)) * 2) + 1)
-        x = 1
-        errs = 1
-        while x != len(transmission):
-            if (x%2!=0): #A -->> B
-                if str(errs) in lost_pkts:
-                    transmission[x] = "error"
-                else:
-                    if(x != 0):
-                        if transmission[x-1] == "timeout" or transmission[x-1] == "error":
-                            transmission[x] = "retry"
-                        else:
-                            transmission[x] = "ok"
+    x = 1
+    errs = 1
+    #percorre o array transmission e muda os valores de strings
+    while x != len(transmission):
+        #como x começa em 1, quando x for impar é transmissão de a para b
+        if (x%2!=0): #A -->> B
+            #se a posição atual estiver na lista de erros marca como "error"
+            if str(errs) in lost_pkts:
+                transmission[x] = "error"
+            #senão verifica se a posição anterior é um timeout ou erro, para ser "retry"
+            #caso não tenha problemas, marca como "ok"
+            else:
+                if(x != 0):
+                    if transmission[x-1] == "timeout" or transmission[x-1] == "error":
+                        transmission[x] = "retry"
                     else:
-                            transmission[x] = "ok"
-                errs +=1
-                    
-            else: #B -->> A
-                
-                if str(errs) in lost_pkts:
-                    transmission[x] = "error"
-                    errs +=1
-                elif transmission[x-1] == "error":
-                    transmission[x] = "timeout"
+                        transmission[x] = "ok"
                 else:
-                    transmission[x] = "ok"
-                    errs +=1
-            x+=1
-        printAuxSaw(transmission)
+                        transmission[x] = "ok"
+            errs +=1
+        #com x par é mensagem de b para a   
+        else: #B -->> A
+            #se a posição atual estiver na lista de erros marca como "error"
+            if str(errs) in lost_pkts:
+                transmission[x] = "error"
+                errs +=1
+            #se a posição anterior for erro marca a atual com "timeout"
+            elif transmission[x-1] == "error":
+                transmission[x] = "timeout"
+            #senão marca com "ok"
+            else:
+                transmission[x] = "ok"
+                errs +=1
+        x+=1
+    #chama func auxiliar para imprimir mensagem na tela
+    printAuxSaw(transmission)
 
 
 def printAuxSaw(transmission):
     x = 1
     frame = False
     n = 1
-    #print(transmission)
+    #percorre todo o array
     while x != len(transmission):
         if(x%2!=0): #A -->> B
-            #print(transmission[x])
+            #mensagem caso "ok"
             if transmission[x]=="ok": 
                 if frame: print("A ->> B : ("+str(n)+") Frame 1")
                 else: print("A ->> B : ("+str(n)+") Frame 0")
-                #frame = not frame
+            #mensagem caso "error"
             elif transmission[x] == "error":
                 if frame: print("A -x B : ("+str(n)+") Frame 1")
                 else: print("A -x B : ("+str(n)+") Frame 0")
+            #mensagem caso "retry"
             elif transmission[x] == "retry":
                 if frame: print("A ->> B : ("+str(n)+") Frame 1 (RET)")
                 else: print("A ->> B : ("+str(n)+") Frame 0 (RET)")
-                #frame = not frame
-                #n+=1
-
+                
         else:   #B -->> A
-            #print(transmission[x])
+            #mensagem caso ok
             if transmission[x]=="ok":
                 if not frame:
                     print("B -->> A : Ack 1")
@@ -110,8 +82,10 @@ def printAuxSaw(transmission):
                     print("B -->> A : Ack 0")
                 frame = not frame
                 n+=1
+            #mensagem caso "timeout"
             elif transmission[x]=="timeout":
                 print("Note over A : TIMEOUT ("+str(n)+")")
+            #mensagem caso "error"
             elif transmission[x]=="error":
                 if not frame:
                     print("B --x A : Ack 1")
